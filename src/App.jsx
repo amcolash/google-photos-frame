a7b60dc50402388d96850fd5fb9d2dfcaadcff68
Andrew McOlash <amcolash@gmail.com>
2022-08-05 09:59:35 -0700

    starting to figure things out

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import Photos from 'googlephotos';
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+export default () => {
+  const [accessToken, setAccessToken] = useState();
+
+  useEffect(async () => {
+    const photos = new Photos(token);
+    const response = await photos.albums.list();
+
+    console.log(response);
+  }, [accessToken]);
+
+  useEffect(() => {
+    const auth = getAuth();
+
+    auth.onAuthStateChanged((user) => {
+      console.log(user);
+
+      if (user.refreshToken) {
+        user
+          .getIdToken(true)
+          .then(function (idToken) {
+            setAccessToken(idToken);
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      } else {
+        const provider = new GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/photoslibrary.readonly');
+
+        signInWithPopup(auth, provider)
+          .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // ...
+
+            setAccessToken(user.accessToken);
+          })
+          .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+
+            console.error(errorCode, errorMessage);
+          });
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+    </>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { initializeApp } from 'firebase/app';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC5Wn-x5E_G3umM2GvRj2cU_Onyy3fEdzY',
+  authDomain: 'photo-frame-21353.firebaseapp.com',
+  projectId: 'photo-frame-21353',
+  storageBucket: 'photo-frame-21353.appspot.com',
+  messagingSenderId: '990708290980',
+  appId: '1:990708290980:web:cff403b9ff1a4a4477dd31',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+ReactDOM.render(<App />, document.getElementById('root'));
