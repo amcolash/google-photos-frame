ab85f7d8ddfbc34bbd75f05bc4773de5bea21ebb
Andrew McOlash <amcolash@gmail.com>
2023-12-12 16:34:29 -0800

    change default background color from white to black

--- src/index.jsx
+++ src/index.jsx
@@ -11,6 +11,8 @@ import { colors, isIpad } from './util';
 if (isIpad()) {
   document.body.style.background = colors.dark;
   document.body.style.color = colors.light;
+} else {
+  document.body.style.background = colors.light;
 }
 
 const root = createRoot(document.getElementById('root'));
